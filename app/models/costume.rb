fb316d9fe3851ddded60af228201e281497090f8
Isabella <wileyisabella@gmail.com>
2021-05-05 13:19:37 -0400

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,6 @@
+class Costume < ActiveRecord::Base
+    has_many :haunted_houses, through: :costume_stores
+end
+
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,5 @@
+class HauntedHouse < ActiveRecord::Base
+    has_many :costumes, through: :costume_stores
+end
+
 # Create your HauntedHouse class here
